b628212a3739e04c660ab5ee5925cdccb19f26ab
yyayourt <yanisnasri30@gmail.com>
2024-06-26 14:53:00 +0200

    correction

--- controllers/users.js
+++ controllers/users.js
@@ -3,21 +3,20 @@ const User = require("../models/users");
 const jwt = require("jsonwebtoken");
 
 exports.signup = (req, res, next) => {
-    exports.signup = (req, res, next) => {
-        bcrypt
-            .hash(req.body.password, 10)
-            .then((hash) => {
-                const user = new User({
-                    email: req.body.email,
-                    password: hash,
-                });
-                user.save()
-                    .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
-                    .catch((error) => res.status(400).json({ error }));
-            })
-            .catch((error) => res.status(500).json({ error }));
-    };
+    bcrypt
+        .hash(req.body.password, 10)
+        .then((hash) => {
+            const user = new User({
+                email: req.body.email,
+                password: hash,
+            });
+            user.save()
+                .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
+                .catch((error) => res.status(400).json({ error }));
+        })
+        .catch((error) => res.status(500).json({ error }));
 };
+
 exports.login = (req, res, next) => {
     User.findOne({ email: req.body.email })
         .then((user) => {
